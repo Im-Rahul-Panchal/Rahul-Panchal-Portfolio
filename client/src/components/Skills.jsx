4f4e3d69095ad4611b49936b1273bac6e7244d5d
Rahul Panchal <rahulpanchal2807@gmail.com>
2026-04-23 15:00:50 +0530

    Add id attribute to Skills section for improved navigation

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -51,7 +51,7 @@ const Skills = () => {
   ];
 
   return (
-    <section className="relative py-32 px-6 bg-[#030712] overflow-hidden" ref={ref}>
+    <section id="skills" className="relative py-18 px-6 bg-[#030712] overflow-hidden" ref={ref}>
       {/* Background Ambient Glows */}
       <div className="absolute top-0 left-1/4 w-96 h-96 bg-indigo-600/10 rounded-full blur-[120px] -z-10" />
       <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-purple-600/10 rounded-full blur-[120px] -z-10" />
